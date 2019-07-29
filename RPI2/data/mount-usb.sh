da307ef6d450b6616dd1b5cfa0219a002ea68ced
Stéphane Stassin <sstassin@gmail.com>
2019-07-29 14:59:36 +0200

    Commit

--- RPI2/data/mount-usb.sh
+++ RPI2/data/mount-usb.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 echo "TRACE-Executing the mount-usb.sh script"
-echo "TRACE-Mounting the /HDD1000G folder...."
-#mkdir -p /mnt/HDD1000G && mount /mnt/HDD1000G
-mount /HDD1000G
+echo "TRACE-Mounting the /mnt/HDD1000G folder...."
+mkdir -p /mnt/HDD1000G && mount /mnt/HDD1000G
 echo "TRACE-Mounted successfully....listing file in mounted directory"
-#ls -l /mnt/HDD1000G
-ls -l /HDD1000G
-echo "TRACE-Will Sleep for one year before exiting........"
-sleep 31536000
+ls -l /mnt/HDD1000G
+echo -e "TRACE-Sleeping forever.."
+while true; do
+   # every hour a "zzz" is put into the log files.
+   echo "TRACE-Sleeping for ten more hours"
+   sleep 36000
+done
 echo "TRACE-a long time has passed away..... exiting"
 
