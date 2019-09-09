0690cfd78b6b77c9f5a934812a43c93b145c62be
Stéphane Stassin <sstassin@gmail.com>
2019-09-09 15:30:22 +0200

    Standard Commit

--- RPI3B/data/src/mount-and-share.sh
+++ RPI3B/data/src/mount-and-share.sh
@@ -28,22 +28,15 @@ echo "   comment=path_ext_mnt_partition1" >> /etc/samba/smb.conf
 testparm --suppress-prompt -c /etc/samba/samba.conf
 #service smbd start
 
-echo -e "******* Listing the Samba shared services *******************"
-#smbclient -L 127.0.0.1
+echo -e "******* Listing the Samba shared services on 192.168.0.10 *******************"
+smbclient -L 192.168.0.10
 echo -e "************************************************************\n"
 #/usr/sbin/rc-update add samba
 #/usr/sbin/rc-service samba start
-whereis smbd
 #smbd: /usr/sbin/smbd
 
-<<<<<<< HEAD:RPI2/data/src/mount-and-share.sh
 echo -e "******* Mounting NFS mount from the Shared HDD1000G *******************"
-#smbclient -L 127.0.0.1
-showmount 192.168.0.10:
+showmount 192.168.0.10
 echo -e "************************************************************\n"
 
-
-=======
-whereis supervisord
->>>>>>> cc793f87d670c4cb997304b95e5984a4e4eb791b:RPI3B/data/src/mount-and-share.sh
 echo "Exiting the mount-and-share.sh script"
--- RPI3B/data/src/start.sh
+++ RPI3B/data/src/start.sh
@@ -2,9 +2,9 @@
 echo "start.sh script"
 /bin/bash /src/mount-and-share.sh
 
-echo "Starting smbd..."
-/usr/sbin/smbd --configfile=/etc/samba/samba.conf
-echo "Exited smbd..."
+#echo "Starting smbd..."
+#/usr/sbin/smbd --configfile=/etc/samba/samba.conf
+#echo "Exited smbd..."
 
 
 while true; do
